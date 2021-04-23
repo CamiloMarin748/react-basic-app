8182b0461ed6968e0c0fca626adef48e468c1d64
Camilo Marin <camilomarin@MacBook-Pro-de-Camilo.local>
2021-04-23 01:01:31 -0500

    first commit

---
+++ src/pages/products/components/filterable-product-table/filterable-product-table.comp.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ProductTable from '../product-table/product-table.comp';
+import SearchBar from '../search-bar/search-bar.comp';
+
+import './filterable-product-table.styles.css';
+
+
+const  FilterableProductTable = () => {
+
+return (
+    <div className= "filterable-product-table-container">  
+    
+    <SearchBar/>
+    <ProductTable/>
+
+    </div>
+  );
+
+ }
+ 
+ export default FilterableProductTable;
---
+++ src/pages/products/products.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import FilterableProductTable from './components/filterable-product-table/filterable-product-table.comp';
+
+const  ProductsPage = () => {
+
+return (
+    <FilterableProductTable/>
+  );
+
+ }
+ 
+ export default ProductsPage;
+ 
